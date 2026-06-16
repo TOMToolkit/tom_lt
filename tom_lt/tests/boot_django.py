4f2af07d8d937a71dbd2622ed0c4ac7fa4c50000
Joey Chatelain <jchate6@gmail.com>
2026-06-15 17:48:14 -0700

    fix lingering generic app names

--- tom_lt/tests/boot_django.py
+++ tom_lt/tests/boot_django.py
@@ -8,7 +8,7 @@ import django
 from django.conf import settings
 from tom_common.default_settings import TOMTOOKIT_INSTALLED_APPS, TOMTOOKIT_MIDDLEWARE
 
-APP_NAME = 'tom_app'  # the stand-alone app we are testing
+APP_NAME = 'tom_lt'  # the stand-alone app we are testing
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), APP_NAME))
 
--- tom_lt/tests/tests.py
+++ tom_lt/tests/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 
 
 class TestApp(TestCase):
-    """NOTE: to run these tests in your venv: python ./{{tom_app}}/tests/run_tests.py"""
+    """NOTE: to run these tests in your venv: python ./tom_lt/tests/run_tests.py"""
 
     def test_unittest(self):
         """Ensure the testing infrastructure is working."""
